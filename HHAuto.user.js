1a6fb2be20806b05933503f30e5f67d62a1c7e2b
Roukys <73461293+Roukys@users.noreply.github.com>
2021-07-20 15:25:47 +0200

    5.5-beta.8 fix pachinko menu issue
    #267

--- HHAuto.user.js
+++ HHAuto.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         HaremHeroes Automatic++ Beta Prod
 // @namespace    https://github.com/Roukys/HHauto
-// @version      5.5-beta.7
+// @version      5.5-beta.8
 // @description  Open the menu in HaremHeroes(topright) to toggle AutoControlls. Supports AutoSalary, AutoContest, AutoMission, AutoQuest, AutoTrollBattle, AutoArenaBattle and AutoPachinko(Free), AutoLeagues, AutoChampions and AutoStatUpgrades. Messages are printed in local console.
 // @author       JD and Dorten(a bit), Roukys, cossname, YotoTheOne
 // @match        http*://nutaku.haremheroes.com/*
@@ -10290,7 +10290,7 @@ var start = function () {
     + '</div>'
     // _End row of 3 columns_
     +'</div>';
-    $('#contains_all').prepend(sMenu);
+    $('#contains_all').parent().prepend(sMenu);
 
     GM_addStyle(''
                 +'#sMenuButton {'
